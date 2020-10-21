e0d56aa93f6cf132b35a32663a00e4950a1be8d8
Diamond Lewis <findlewis@gmail.com>
2020-10-21 16:51:23 -0500

    chore: Dump Dependencies (#775)
    * gem dependencies
    
    * node dependencies
    
    * Update .gitignore
    
    * Add License

--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,8 @@ module.exports = {
         exclude: /node_modules/,
         use: { 
           loader: 'babel-loader', 
-          query: {
-            presets: ['es2015', 'react']
+          options: {
+            presets: ['@babel/preset-env']
           }
         },
       },
